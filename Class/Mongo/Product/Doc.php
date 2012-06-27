a10b8b768eaf58fbbfa700dbc148959d84ea50c9
Gavin Li <craftgavin@gmail.com>
2012-06-27 15:48:23 +0800

    added field to product

--- Class/Mongo/Product/Doc.php
+++ Class/Mongo/Product/Doc.php
@@ -1,5 +1,18 @@
 <?php
 class Class_Mongo_Product_Doc extends App_Mongo_Db_Document
 {
-	
+	protected $_field = array(
+		'attributesetId',
+		'fulltext',
+		'groupId',
+		'introicon',
+		'introtext',
+		'label',
+		'name',
+		'sku',
+		'origPrice',
+		'price',
+		'showWhere',
+		'weight'
+	);
 }
