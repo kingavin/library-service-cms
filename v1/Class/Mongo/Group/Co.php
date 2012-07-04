6ad4b042c2cce0f59a15f5748c0ad17960b6616f
Gavin Li <craftgavin@gmail.com>
2012-07-04 11:32:32 +0800

    create group if not existed

--- v1/Class/Mongo/Group/Co.php
+++ v1/Class/Mongo/Group/Co.php
@@ -10,8 +10,14 @@ class Class_Mongo_Group_Co extends App_Mongo_Db_Collection
 	public function findArticleGroup()
 	{
 		if(is_null(self::$_articleDoc)) {
-			self::$_articleDoc = $this->addFilter('type', 'article')
+			$aDoc = $this->addFilter('type', 'article')
 				->fetchOne();
+			if(is_null($aDoc)) {
+				$aDoc = $this->create();
+				$aDoc->type = 'article';
+				$aDoc->save();
+			}
+			self::$_articleDoc = $aDoc;
 		}
 		return self::$_articleDoc;
 	}
@@ -19,8 +25,14 @@ class Class_Mongo_Group_Co extends App_Mongo_Db_Collection
 	public function findProductGroup()
 	{
 		if(is_null(self::$_productDoc)) {
-			self::$_productDoc = $this->addFilter('type', 'product')
+			$pDoc = $this->addFilter('type', 'product')
 				->fetchOne();
+			if(is_null($pDoc)) {
+				$pDoc = $this->create();
+				$pDoc->type = 'product';
+				$pDoc->save();
+			}
+			self::$_productDoc = $pDoc;
 		}
 		return self::$_productDoc;
 	}
