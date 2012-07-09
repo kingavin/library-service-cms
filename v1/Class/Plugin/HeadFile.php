8324e3e678e31423d0ccc11bcad5a8223581372d
Gavin Li <craftgavin@gmail.com>
2012-07-09 18:11:33 +0800

    head files

--- v1/Class/Plugin/HeadFile.php
+++ v1/Class/Plugin/HeadFile.php
@@ -1,20 +1,27 @@
 <?php
 class Class_Plugin_HeadFile extends Zend_Controller_Plugin_Abstract
 {
-    public function preDispatch(Zend_Controller_Request_Abstract $request)
-    {
+	public function preDispatch(Zend_Controller_Request_Abstract $request)
+	{
 		if($request->getModuleName() != 'admin' && $request->getModuleName() != 'rest') {
+			$view = new Zend_View();
+			$csa = Class_Session_Admin::getInstance();
+			
+			$fileUrl = Class_Server::fileUrl();
+			if($csa->getSessionData('localCssMode') == 'active') {
+				$fileUrl = 'http://local.host/'.Class_Server::getOrgCode();
+			}
+			
 			$headFileCo = App_Factory::_m('HeadFile');
-	    	$headFileDocs = $headFileCo->fetchDoc();
-	    	
-	    	foreach($headFileDocs as $doc) {
-	    		if($doc->type == 'css') {
-	    			$view = new Zend_View();
-	    			$view->headLink()->appendStylesheet(Class_Server::fileUrl().'/'.$doc->filename);
-	    		} else {
-	    			$view->headScript()->appendFile(Class_Server::fileUrl().'/'.$doc->filename);
-	    		}
-	    	}
+			$headFileDocs = $headFileCo->fetchDoc();
+			
+			foreach($headFileDocs as $doc) {
+				if($doc->type == 'css') {
+					$view->headLink()->appendStylesheet($fileUrl.'/'.$doc->filename);
+				} else {
+					$view->headScript()->appendFile($fileUrl.'/'.$doc->filename);
+				}
+			}
 		}
-    }
+	}
 }
