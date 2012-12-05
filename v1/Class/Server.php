fa0f28b82b27835e3462284ba085e5ee0f9c5e66
Gavin Li <craftgavin@gmail.com>
2012-12-05 17:08:01 +0800

    use new url

--- v1/Class/Server.php
+++ v1/Class/Server.php
@@ -28,7 +28,7 @@ class Class_Server
 	
 	public static function getImageUrl()
 	{
-		return 'http://storage.aliyun.com/public-misc';
+		return 'http://misc.fucms.com/public-misc';
 	}
 	
 	public static function getImageFolderUrl()
@@ -110,7 +110,7 @@ class Class_Server
 	
 	public static function fileUrl()
 	{
-		return 'http://storage.aliyun.com/public-misc/'.self::$_orgCode;
+		return 'http://misc.fucms.com/public-misc/'.self::$_orgCode;
 	}
 	
 	public static function getFileServer()
